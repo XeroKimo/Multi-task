43d3fef9338befb556111fd301ad818cdb118671
Jean Alarcon <Renzy.Alarcon@hotmail.com>
2020-07-12 15:18:51 -0400

    - Created Town and enemy script - Enemies can now traverse a path and reach the Town.cs

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿//Module: Enemy
+//Version: 0.11
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int damage;
+    public float speed;
+    public Town target;
+    public Spline2DComponent path;
+
+    public float distanceTraveled = 0;
+
+    public void OnEnable()
+    {
+        transform.position = path.GetPointWorldSpace(0);
+        distanceTraveled = 0;
+    }
+
+    public void Update()
+    {
+        distanceTraveled += speed * Time.deltaTime;
+        transform.position = path.InterpolateDistanceWorldSpace(distanceTraveled);
+
+        if(distanceTraveled > path.Length)
+        {
+            target.ApplyDamage(damage);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Town.cs
@@ -0,0 +1,28 @@
+﻿//Module: Lives
+//Version: 0.1
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Town : MonoBehaviour
+{
+    [SerializeField]
+    int m_health;
+
+    public Spline2DComponent[] paths;
+
+    public delegate void OnDeath();
+    public event OnDeath onDeathEvent;
+
+    public void ApplyDamage(int damage)
+    {
+        m_health -= damage;
+        if(m_health < 0)
+        {
+            m_health = 0;
+            onDeathEvent?.Invoke();
+        }
+    }
+}
