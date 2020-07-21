402151804f305ed70928865f46da31eb2f5ec8b5
Jean Alarcon <Renzy.Alarcon@hotmail.com>
2020-07-21 14:04:35 -0400

    - Enemies have been updated to have 2 different events, reaching the end of the path, and dying

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,5 @@
 ﻿//Module: Enemy
-//Version: 0.11
+//Version: 0.13
 
 using System.Collections;
 using System.Collections.Generic;
@@ -10,10 +10,16 @@ public class Enemy : MonoBehaviour
 {
     public int damage;
     public float speed;
-    public Town target;
-    public Spline2DComponent path;
+    public int money;
+    public int m_health { get; private set; }
 
+    public Spline2DComponent path;
     public float distanceTraveled = 0;
+
+    public delegate void OnDeath(Enemy enemy);
+    public event OnDeath onDeath;
+    public event OnDeath onPathFinished;
+
 
     public void OnEnable()
     {
@@ -28,8 +34,23 @@ public class Enemy : MonoBehaviour
 
         if(distanceTraveled > path.Length)
         {
-            target.ApplyDamage(damage);
+            onPathFinished?.Invoke(this);
             gameObject.SetActive(false);
+        }
+    }
+
+    public void SetHealth(int health)
+    {
+        Debug.Assert(health > 0, "Setting enemy health must be greater than 0");
+        m_health = health;
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        m_health -= damage;
+        if(m_health < 0)
+        {
+            onDeath?.Invoke(this);
         }
     }
 }
