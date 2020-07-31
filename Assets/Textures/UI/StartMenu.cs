e931adcfb11e0f4e058aa3319e926ed7397b98ac
Nick Truong <nicktruong@outlook.com>
2020-07-31 19:49:55 -0400

    Barebones main menu.

---
+++ Assets/Textures/UI/StartMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject levelSelect;
+    public GameObject startButton;
+
+    void Start()
+    {
+        levelSelect.SetActive(false);
+        startButton.GetComponent<Button>().onClick.AddListener(StartOnClick);
+    }
+
+    void StartOnClick()
+    {
+        levelSelect.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+}
