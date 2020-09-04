2ddcdc89455ccd43d1723521ddd6cb663e9ed008
Nick Truong <nicktruong@outlook.com>
2020-09-04 12:50:31 -0400

    ProjectilePool functional, EnemyPool implemented

---
+++ Assets/Scripts/EnemyPool.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPool : Pool
+{
+    static EnemyPool instance;
+
+    public override void Awake()
+    {
+        if (instance) return;
+        instance = this;
+
+        base.Awake();
+    }
+}
---
+++ Assets/Scripts/ProjectilePool.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectilePool : Pool
+{
+    public static ProjectilePool instance;
+
+    public override void Awake()
+    {
+        if (instance) return;
+        instance = this;
+
+        base.Awake();
+    }
+}
